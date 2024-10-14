c3436eedbb83e94ea979d90878129a277fbc0faa
Your Name <you@example.com>
2024-10-14 15:20:32 +0300

    completed tasks

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.time.LocalDate;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else {
+            return year % 4 == 0;
         }
+    }
+
+    public static String getRecommendations(int clientOS, int year) {
+        String recommendation = "";
+        if (year <= 2015) {
+            if (clientOS == 1) {
+                recommendation = "Установите облегченную версию приложения для Android по ссылке";
+            } else if (clientOS == 0) {
+                recommendation = "Установите облегченную версию приложения для iOS по ссылке";
+            }
+        } else {
+            if (clientOS == 1) {
+                recommendation = "Установите версию приложения для Android по ссылке";
+            } else if (clientOS == 0) {
+                recommendation = "Установите версию приложения для iOS по ссылке";
+            }
+        }
+        return recommendation;
+    }
+
+    public static String getDaysToDelivery(int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            return "Потребуется дней: 1";
+        } else if (deliveryDistance <= 60) {
+            return "Потребуется дней: 2";
+        } else if (deliveryDistance <= 100) {
+            return "Потребуется дней: 3";
+        } else {
+            return "Доставка недоступна";
+        }
+    }
+
+    public static void main(String[] args) {
+        // Task 1
+        int year = 2021;
+        if (isLeapYear(year)) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
+
+        System.out.println();
+        // Task 2
+        int currentYear = LocalDate.now().getYear();
+        int clientOS = 1;
+        System.out.println(getRecommendations(clientOS, currentYear));
+
+        System.out.println();
+        //Task 3
+        int deliveryDistance = 95;
+        System.out.println(getDaysToDelivery(deliveryDistance));
+
     }
 }
